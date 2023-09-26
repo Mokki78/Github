b906df0d62e5ba5a41e76a7ad91287f33ab8d341
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2023-09-26 12:25:53 +0200

    Added products api

--- react-app-1/src/pages/home.jsx
+++ react-app-1/src/pages/home.jsx
@@ -1,11 +1,58 @@
-import { Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
 
 export function Home() {
-    return (
-        <Row {md=4}>
-           {products.map(product => (
-                <Col></Col>
-            ))}
-        </Row>
-    )
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setPosts(json);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
+
+  return (
+    <div>
+      {posts.map((post,i) => (
+            <Button
+            key={i}
+              onClick={(e) => {
+                
+              }}>
+        <div>
+          <h2>{post.title}</h2>
+          <img src={post.imageUrl} height="200px" alt={post.title} />
+          <p>{post.description}</p>
+          <p>Price: {post.discountedPrice}</p>
+          <p>Discounter price: {post.discountedPrice}</p>
+          <p>Rating: {post.rating}</p>
+        </div>
+        </Button>
+      ))}
+     </div>
+  );
 }
+
+export default Home;
